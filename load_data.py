22b672b3137b6f1a63adc61008853d1108e3db52
Michael Mezzina <michaelmez39@gmail.com>
2022-03-09 10:47:01 -0500

    Loads the image and label dataset into tensorflow Dataset

--- load_data.py
+++ load_data.py
@@ -10,29 +10,40 @@ img_width = 600
 data_dir = pathlib.Path("../Images/MBM")
 batch_size = 32
 
-def make_label(dots):
-  kernel_in = np.ones((32, 32, 1, 1))
-  kernel = tf.constant(kernel_in, dtype=tf.float32) 
-  return tf.nn.conv2d(dots, kernel, strides=[1,1,1,1], padding="VALID", data_format="NHWC")
 
-# list_ds_image = tf.data.Dataset.list_files(str(data_dir/'image/*'))
-# list_ds_label = tf.data.Dataset.list_files(str(data_dir/'label/*'))
-# length_image = list_ds_image.cardinality().numpy()
-# length_label = list_ds_label.cardinality().numpy()
-# print(f"Labels: {length_label}\nImages:{length_image}")
-# assert length_image == length_label
+class CellDataset:
+    def __init__(self, data_dir, img_width, img_height):
+        self.data_dir = pathlib.Path(data_dir)
+        self.img_shape = (img_width, img_height)
+        self.images = keras.utils.image_dataset_from_directory(
+            str(self.data_dir/'image'), label_mode=None, color_mode="rgb", image_size=self.img_shape)
+        self.labels = keras.utils.image_dataset_from_directory(
+            str(self.data_dir/'label'),
+            label_mode=None,
+            color_mode="grayscale",
+            image_size=self.img_shape).map(self.make_label
+            ).map(layers.Rescaling(1./255))
+        self.num_images = self.images.cardinality().numpy()
+        num_labels = self.labels.cardinality().numpy()
+        assert self.num_images == num_labels
+    """
+    Turns point label image into density map for training
+    """
+    def make_label(_, dots):
+        kernel_in = np.ones((32, 32, 1, 1))
+        kernel = tf.constant(kernel_in, dtype=tf.float32)
+        print(dots.shape)
+        return tf.nn.conv2d(dots, kernel, strides=[1, 1, 1, 1], padding="VALID")
 
-# list_ds_label = list_ds_label.map(make_label)
 
-images = (data_dir/'label').iterdir()
-print(images)
-im = PIL.Image.open(str(list(images)[0]))
-filtered = make_label(np.array(im.getdata()).reshape((1, 600, 600, 1)))
-scaled = np.array(filtered).reshape(569, 569) * 20
-im = PIL.Image.fromarray(scaled.astype(np.uint8))
-im.show()
+def main():
+    dataset = CellDataset("../Images/MBM", 600, 600)
+    # This will display the first image in the dataset as a heatmap
+    first = [a for a in dataset.labels.as_numpy_iterator()][0]
+    scaled = first * 20
+    im = PIL.Image.fromarray(scaled.astype(np.uint8).reshape((32, 569, 569))[1, :,:])
+    im.show()
 
-"""
-Turns point label image into density map for training
-"""
 
+if __name__ == '__main__':
+    main()
