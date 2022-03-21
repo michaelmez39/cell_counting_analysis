fa4159ffb1751d6db9b14b2da01ef2bfa57d38c7
Michael Mezzina <michaelmez39@gmail.com>
2022-03-21 10:46:10 -0400

    Model and dataset are made, difficulties in training model (actually add changes)

--- count.py
+++ count.py
@@ -1,35 +1,64 @@
 import tensorflow as tf
+
 from tensorflow import keras
 from tensorflow.keras import layers
+from load_data import CellDataset
 
+class ConvBlock(keras.Model):
+  def __init__(self, filter1, filter3):
+    super(ConvBlock, self).__init__()
+    self.conv1 = layers.Conv2D(filter1, 1, 1, padding="SAME")
+    self.conv3 = layers.Conv2D(filter3, 3, 1, padding="SAME")
+    self.bn = layers.BatchNormalization()
+    self.activation = layers.LeakyReLU()
+    self.concat = layers.Concatenate(axis=0)
+
+  def call(self, inputs, training=False):
+    x1 = self.conv1(inputs)
+    x1 = self.bn(x1, training=training)
+    x1 = self.activation(x1)
+
+    x2 = self.conv1(inputs)
+    x2 = self.bn(x2, training=training)
+    x2 = self.activation(x2)
+
+    x = self.concat([x1, x2])
+    return x
+
+# Layer(3, 64, 3, "valid"),
+# FilterBlock(64, 16, 16),
+# FilterBlock(32, 16, 32),
+# Layer(48, 16, 14, "valid"),
+# FilterBlock(16, 112, 48),
+# FilterBlock(160, 40, 40),
+# FilterBlock(80, 32, 96),
+# Layer(128, 16, 17, "valid"),
+# Layer(16, 64, 1, "valid"),
+# Layer(64, 1, 1, "valid")
 
 model = keras.Sequential([
+  layers.ZeroPadding2D(padding=(16, 16)),
   layers.Rescaling(1./255),
-  layers.Conv2d(64, 3),
+  layers.Conv2D(64, 3, padding="VALID"),
   layers.BatchNormalization(),
-  layers.Conv2d(16, 1, activation="selu"),
-  layers.Conv2d(16, 3, padding="same", activation="selu"),
+  layers.LeakyReLU(),
+  ConvBlock(16, 16),
+  ConvBlock(16, 32),
+  layers.Conv2D(16, 14, padding="VALID"),
   layers.BatchNormalization(),
-  layers.Conv2d(16, 1, activation="selu"),
-  layers.Conv2d(32, 3, padding="same", activation="selu"),
+  layers.LeakyReLU(),
+  ConvBlock(112, 48),
+  ConvBlock(40, 40),
+  ConvBlock(32, 96),
+  layers.Conv2D(16, 17, padding="VALID"),
   layers.BatchNormalization(),
-  layers.Conv2d(16, 14, activation="selu"),
+  layers.LeakyReLU(),
+  layers.Conv2D(64, 1, padding="VALID"),
   layers.BatchNormalization(),
-  layers.Conv2d(112, 1, activation="selu"),
-  layers.Conv2d(48, 3, padding="same", activation="selu"),
+  layers.LeakyReLU(),
+  layers.Conv2D(1, 1, padding="VALID"),
   layers.BatchNormalization(),
-  layers.Conv2d(40, 1),
-  layers.Conv2d(40, 3, padding="same", activation="selu"),
-  layers.BatchNormalization(),
-  layers.Conv2d(32, 1, activation="selu"),
-  layers.Conv2d(96, 3, padding="same", activation="selu"),
-  layers.BatchNormalization(),
-  layers.Conv2d(16, 17, activation="selu"),
-  layers.BatchNormalization(),
-  layers.Conv2d(64, 1, activation="selu"),
-  layers.BatchNormalization(),
-  layers.Conv2d(64, 1, activation="selu"),
-  layers.BatchNormalization(),
+  layers.LeakyReLU(),
 ])
 
 model.compile(
@@ -38,6 +67,11 @@ model.compile(
   metrics=[keras.metrics.Accuracy()]
 )
 
-model.fit(
+dataset = CellDataset("../Images/MBM")
 
+model.fit(
+  dataset.data.batch(1),
+  epochs=2,
 )
+
+model.save("model0")
--- load_data.py
+++ load_data.py
@@ -1,38 +1,69 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
 import numpy as np
-import PIL
 import pathlib
 
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.data import Dataset
+from PIL import Image
 img_height = 600
 img_width = 600
-data_dir = pathlib.Path("../Images/MBM")
+data_dir = "../Images/MBM"
 batch_size = 32
 
-def make_label(dots):
-  kernel_in = np.ones((32, 32, 1, 1))
-  kernel = tf.constant(kernel_in, dtype=tf.float32) 
-  return tf.nn.conv2d(dots, kernel, strides=[1,1,1,1], padding="VALID", data_format="NHWC")
 
-# list_ds_image = tf.data.Dataset.list_files(str(data_dir/'image/*'))
-# list_ds_label = tf.data.Dataset.list_files(str(data_dir/'label/*'))
-# length_image = list_ds_image.cardinality().numpy()
-# length_label = list_ds_label.cardinality().numpy()
-# print(f"Labels: {length_label}\nImages:{length_image}")
-# assert length_image == length_label
+class CellDataset:
+  def __init__(self, path):
+    self.root_dir = pathlib.Path(path)
+    self.images = self.root_dir/"image"
+    self.labels = self.root_dir/"label"
+    l = [(str(i), str(l)) for i, l in zip(self.images.iterdir(), self.labels.iterdir())]
+    ds = Dataset.from_tensor_slices(l)
+    self.data = ds.map(self.process_path)
+    return
 
-# list_ds_label = list_ds_label.map(make_label)
+  """
+  Turns point label image into density map for training
+  """
+  def make_label(self, dots):
+    #resize = tf.constant([1, 600, 600, 1], dtype=tf.int32)
+    #img = tf.reshape(dots, resize)
+    img = tf.cast(dots, tf.float32)
 
-images = (data_dir/'label').iterdir()
-print(images)
-im = PIL.Image.open(str(list(images)[0]))
-filtered = make_label(np.array(im.getdata()).reshape((1, 600, 600, 1)))
-scaled = np.array(filtered).reshape(569, 569) * 20
-im = PIL.Image.fromarray(scaled.astype(np.uint8))
-im.show()
+    kernel_in = np.ones((32, 32, 1, 1))
+    kernel = tf.constant(kernel_in, dtype=tf.float32)
+    img = tf.image.resize_with_pad(img, 632, 632)
+    img = tf.reshape(img, (1, 632, 632, 1))
+    img =  tf.nn.conv2d(img, kernel, 1, padding="VALID")
+    img = tf.reshape(img, (601, 601, 1))
+    return img
 
-"""
-Turns point label image into density map for training
-"""
+  def process_path(self, paths):
+    # resize1 = tf.constant([1, 600, 600, 3])
+    # resize2 = tf.constant([1, 601, 601, 1])
+
+    img_raw = tf.io.read_file(paths[0])
+    img = tf.image.decode_png(img_raw, channels=3)
+
+
+    lbl_raw = tf.io.read_file(paths[1])
+    lbl = self.make_label(tf.image.decode_png(lbl_raw, channels=1))
+    lbl.set_shape([601,601,1])
+    return img, lbl
+
+
+def main():
+  ds = CellDataset(data_dir)
+
+  il = ds.data.take(1)
+
+  for img, lbl in il:
+    Image.fromarray(img.numpy()).show()
+    resize = tf.constant([600, 600])
+    lbl = tf.cast(lbl * 20, tf.uint8)
+    lbl = tf.reshape(lbl, resize).numpy()
+    Image.fromarray(lbl, mode="L").show()
+
+if __name__ == "main":
+  main()
 
